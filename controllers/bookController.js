70045ec8274832fa6d6aef59096d3212bf868684
Alan Cruz <alanchis@hotmail.com>
2022-02-03 13:13:15 -0600

    formulario

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,19 +2,24 @@ const Book		= require("./../models/Book")
 
 exports.getBooks = async (req, res) => {
 
-    try {
-        const foundBooks = await Book.find({})
+	try {
+	
+		const foundBooks = await Book.find({})
 
-        console.log(foundBooks)
-    
-        res.render("books/list",{
-            data: foundBooks
-        })
-    
+		res.render("books/list", {
+			data: foundBooks
+		})
 
+	} catch (error) {
+		
+		console.log(error)
 
-    } catch (error) {
-        console.log(error)
-    }
+	}	
+
+}
+
+exports.createBooks = async (req, res) => {
+	
+	return res.render("books/create")
 
 }
--- routes/books.js
+++ routes/books.js
@@ -3,6 +3,14 @@ const router			= express.Router()
 
 const bookController	= require("./../controllers/bookController")
 
+
+//obtener libros
 router.get("/", bookController.getBooks)
 
+
+//crear pagina para crear un libros en base de DATOS
+router.get("/create", bookController.createBooks)
+
 module.exports = router
+
+
